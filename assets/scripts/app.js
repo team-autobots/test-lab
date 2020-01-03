71b81925bb2af020e5bb64e3508df107eafac88d
michaelchun <michael.chun03@gmail.com>
2020-01-03 15:13:41 -0500

    Finish get api books

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,21 @@
-'use strict'
+"use strict";
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+const getBook = index()
+  .then(res => console.log(res))
+  .catch(err => console.warn(err));
+
+const index = () => {
+  return $.ajax({
+    url: "https://wdi-library-api.herokuapp.com/books",
+    method: "GET"
+  });
+};
 
 $(() => {
-  // your JS code goes here
-})
+  $("#get-book").on("submit", getBook);
+});
